7e2c096ba0406ebafd55244baeb5726fa8c2941e
Sadita Ahmed <2304007a@student.gla.ac.uk>
2018-01-23 23:03:51 +0000

    Ch8 All test true & OK

--- rango/views.py
+++ rango/views.py
@@ -15,7 +15,6 @@ def index(request):
 
 def show_category(request, category_name_slug):
     context_dict = {}
-
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
